a3fa9e637c456387bfc2bb0d7fd255761384ee8c
Brewalan Le Dru <brewalan@yahoo.com>
2022-11-25 22:15:30 +0100

    footer

--- src/components/template/footer.js
+++ src/components/template/footer.js
@@ -1,10 +1,11 @@
 import * as React from 'react'
 import { Link } from 'gatsby'
-
+import { IconInfo, IconCookie, IconEmail, IconHome } from './iconList'
 import { siteFooter,
 navLinkItem,
 navLinks,
-navLinkText
+navLinkText,
+navLinkLien
 } from './footer.module.scss'
 
 const FooterPage = () => {
@@ -15,17 +16,26 @@ const FooterPage = () => {
         <ul className={navLinks}>
           <li className={navLinkItem}>
             <Link to="/" className={navLinkText}>
-              Accueil
+              <IconHome />
+              <span className={navLinkLien}>Accueil</span>                            
             </Link>
           </li>
           <li className={navLinkItem}>
             <Link to="/about" className={navLinkText}>
-              À propos
+              <IconInfo />
+              <span className={navLinkLien}>À propos</span>                            
             </Link>
           </li>
           <li className={navLinkItem}>
             <Link to="/confidentialite" className={navLinkText}>
-              Confidentialité
+              <IconCookie />
+              <span className={navLinkLien}>Confidentialité</span>
+            </Link>
+          </li>
+          <li className={navLinkItem}>
+            <Link to="/" className={navLinkText}>
+              <IconEmail />
+              <span className={navLinkLien}>Contact</span>              
             </Link>
           </li>
 
--- src/components/template/iconList.js
+++ src/components/template/iconList.js
@@ -1,45 +1,20 @@
 import * as React from 'react'
 import iconCopySvg from "../../images/copy-light.svg"
+import iconAddressSvg from "../../images/address-card-light.svg"
+import iconInfoSvg from '../../images/circle-info-light.svg'
+import iconCookieSvg from "../../images/cookie-light.svg"
+import iconEmailSvg from "../../images/envelope-light.svg"
+import iconGavelSvg from "../../images/gavel-light.svg"
+import iconHomeSvg from "../../images/house-light.svg"
 
-
-
-const IconCopy = () => <img alt="Copier" src={iconCopySvg} width="24px" height="24px" />
-
-
-
-
-
-    {/*
-
-const iconDivCopy = {
-    backgroundColor: "red",
-    mask: "url(../../images/copy-light.svg) no-repeat center"
-}
-{
-    return (
-      <div style={iconDivCopy}></div>
-    )
-  }
-
-
-
-<img alt="Copier" src={iconCopySvg} width="24px" height="24px" />
-    
-    
-    
-    
-</p><img src={iconCopySvg} width="24px" height="24px" />
-
-
-
-
-const IconCopy = () => {
-
-  return (
-    <p>test</p>
-  )
-}
-*/}
+/* list all available icons */
+export const IconCopy = () => <img alt="Copier" src={iconCopySvg} width="24px" height="24px" />
+export const IconAddress = () => <img alt="Adresse" src={iconAddressSvg} width="27px" height="24px" />
+export const IconInfo = () => <img alt="Info" src={iconInfoSvg} width="24px" height="24px" />
+export const IconCookie = () => <img alt="Cookie" src={iconCookieSvg} width="24px" height="24px" />
+export const IconEmail = () => <img alt="Email" src={iconEmailSvg} width="24px" height="24px" />
+export const IconGavel = () => <img alt="Légal" src={iconGavelSvg} width="24px" height="24px" />
+export const IconHome = () => <img alt="Accueil" src={iconHomeSvg} width="27px" height="24px" />
 
 
 export default IconCopy
